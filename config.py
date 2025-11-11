dc005089847773f5e21cabef40d1a2c0b3be6ee9
EVEdge <jbekius@outlook.com>
2025-11-11 11:30:33 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
 
 AFM_URL = os.getenv(
     "AFM_URL",
-    "https://www.afm.nl/nl-nl/sector/registers/meldingenregisters/substantiele-deelnemingen",
+    "https://www.afm.nl/nl-nl/sector/registers/meldingenregisters/netto-shortposities-actueel",
 )
 
 DATABASE_URL   = os.getenv("DATABASE_URL", "sqlite:///afm.db")
-CATEGORY_ID    = int(os.getenv("WP_CATEGORY_ID", "775"))
+CATEGORY_ID    = int(os.getenv("WP_CATEGORY_ID", "777"))
 PUBLISH_STATUS = os.getenv("WP_PUBLISH_STATUS", "publish")  # ← default to draft
 MAX_POSTS_PER_RUN = int(os.getenv("MAX_POSTS_PER_RUN", "10"))
